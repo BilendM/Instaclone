f16d12d11d4aec876cf97c806232a347fa25c71e
Bilendm <bilendmohammed@gmail.com>
2020-05-31 15:18:25 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fbgw4-2.fna.fbcdn.net/v/t51.2885-19/s150x150/17883107_1258302477621562_1355473579625414656_a.jpg?_nc_ht=instagram.fbgw4-2.fna.fbcdn.net&_nc_ohc=un1ykmmUHRYAX8dHP1S&oh=2c7c5c050c0211448c6677d40e5ebf6a&oe=5EFD789B" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 p-5">
+            <div class="">
+                <h1>bilend<h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>91 </strong>Posts</div>
+                <div class="pr-5"><strong>540 </strong>Followers</div>
+                <div class="pr-5"><strong>754 </strong>Following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Bilend Mohammed</div>
+            <div class="">🦉</div>
+            <div class=""><a href="#" class="">Bilend Mohammed's link</a></div>
+        </div>
+        <div class="col-4"><img src="" alt=""></div>
+    </div>
+</div>
+@endsection
